38cfc1f27aaaf2173e4c5b843a91e00c813fbc6a
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-12-21 10:58:19 +0300

    create a react component with ES6 syntax

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // import React and ReactDOM
 import React from 'react';
 import ReactDOM from 'react-dom';
-// create a react component
 
+// create a react component
+const App = () =>{
+  return <div>Hi there</div>;
+}
 //take the react component and show it on the screen
